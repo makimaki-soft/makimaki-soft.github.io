0cde8043a632624170d0e47761e5b118a52e1a0a
sryoji <sryoji5087@gmail.com>
2015-05-16 14:05:34 +0900

    お知らせをjsonから読むように

---
+++ js/makimaki.js
@@ -0,0 +1,23 @@
+function InfoController() {}
+
+InfoController.prototype.load = function() {
+	var self = this;
+	$.getJSON("./data/info.json" , function(data) {
+		self.show(data);
+	});
+}
+
+InfoController.prototype.show = function(json) {
+	// for each
+	var card_info = json[0];
+	var card_obj = $(".info-card")[0];
+	card_obj.querySelector("u").innerText = card_info.date;
+	card_obj.querySelector("p").innerHTML = card_info.body;
+	card_obj.querySelector("img").src = card_info.img;
+}
+
+// ---------------------------------------------------------
+$(function() {
+	var info_controller = new InfoController();
+	info_controller.load();
+});
